6f166c2dbe35dedcf859d24ffc11c808a42a3dca
tu1511 <131641671+tu1511@users.noreply.github.com>
2025-01-09 23:25:57 +0700

    update

--- src/redux/thunk/userThunk.js
+++ src/redux/thunk/userThunk.js
@@ -6,11 +6,9 @@ export const getLoggedInUser = createAsyncThunk(
   "users/getLoggedInUser",
   async (accessToken, thunkAPI) => {
     try {
-      // Gọi API để lấy thông tin người dùng đăng nhập hiện tại dựa trên accessToken
       const response = await userService.getLoggedInUser();
       return response.data;
     } catch (error) {
-      // Trả về lỗi nếu có, sử dụng rejectWithValue để giữ thông tin lỗi
       return thunkAPI.rejectWithValue(
         error.response?.data?.error || error.message
       );
